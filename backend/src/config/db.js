98ac070989963a7a51c335d4a9ee53d097bc2990
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-16 07:44:08 +0300

    update db configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -12,16 +12,4 @@ const connectDB = async () => {
   }
 };
 
-mongoose.connection.on("disconnected", () => {
-  console.warn("MongoDB disconnected. Attempting to reconnect...");
-});
-
-mongoose.connection.on("reconnected", () => {
-  console.log("MongoDB reconnected.");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(`MongoDB connection error: ${err.message}`);
-});
-
 module.exports = connectDB;
